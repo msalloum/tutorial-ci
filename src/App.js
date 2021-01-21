ea5fb3af4ea12772bdc8a933e3753f71c703abad
Mariam <mariam.salloum@gmail.com>
2021-01-21 03:39:13 -0800

    fixing a typo

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React blah blah blah
+          Learn React
         </a>
       </header>
     </div>
